4e29531685a52d5a86cf2780017469f7ac5bd940
Minh1509 <nguyenquangminh15092003@gmail.com>
2025-11-23 10:48:54 +0700

    update

--- chatbot-service/src/modules/data-mapping/data-mapping.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DataMappingService } from './data-mapping.service';
-
-@Module({
-  providers: [DataMappingService],
-  exports: [DataMappingService],
-})
-export class DataMappingModule {}
-
