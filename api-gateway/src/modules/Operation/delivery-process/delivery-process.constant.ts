7ddc66b63a7fb0a24c9e62eaa887b5597ce4193d
trungduck45 <letrungducabcxyz@gmail.com>
2025-12-19 22:42:18 +0700

    update delivery

--- api-gateway/src/modules/Operation/delivery-process/delivery-process.constant.ts
+++ api-gateway/src/modules/Operation/delivery-process/delivery-process.constant.ts
@@ -1,5 +1,5 @@
 export const DELIVERY_PROCESS_CONSTANTS = {
   CREATE_DELIVERY_PROCESS: 'delivery_process.create',
-  GET_ALL_DELIVERY_PROCESS: 'delivery_process.get_all_by_do_id',
+  GET_ALL_DELIVERY_PROCESS: 'delivery_process.get_all_by_do',
   UPDATE_DELIVERY_PROCESS: 'delivery_process.update',
 };
