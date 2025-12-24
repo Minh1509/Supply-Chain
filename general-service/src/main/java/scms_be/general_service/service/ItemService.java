3fb20b438086d2dd573e0ff8926a0e46d2e24b1f
Sotatek-MinhNguyen25 <minh.nguyen25@sotatek.com>
2025-12-24 16:27:35 +0700

    update performence

--- general-service/src/main/java/scms_be/general_service/service/ItemService.java
+++ general-service/src/main/java/scms_be/general_service/service/ItemService.java
@@ -1,5 +1,6 @@
 package scms_be.general_service.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -58,7 +59,7 @@ public class ItemService {
 
   public List<ItemDto> getItemsByIds(List<Long> itemIds) {
     if (itemIds == null || itemIds.isEmpty()) {
-      return List.of();
+      return new ArrayList<>();
     }
     List<Item> items = itemRepository.findAllById(itemIds);
     return items.stream().map(this::convertToDto).collect(Collectors.toList());
